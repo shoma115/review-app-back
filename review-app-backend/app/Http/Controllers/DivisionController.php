e3d5e4afef613b0857e15fa60c18bed994bfc7d3
shoma115 <xiangzhendaogen8@gmail.com>
2023-12-04 19:57:29 +0900

    モデルとコントローラを作成

---
+++ review-app-backend/app/Http/Controllers/DivisionController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Division;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class DivisionController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Division $division)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Division $division)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Division $division)
+    {
+        //
+    }
+}
---
+++ review-app-backend/app/Http/Controllers/LessonController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Lesson;
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class LessonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Lesson $lesson)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Lesson $lesson)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Lesson $lesson)
+    {
+        //
+    }
+}
