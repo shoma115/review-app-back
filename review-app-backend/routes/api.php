d8d27e1c11a6f595e7787bd70bf2e7e9cc3963f1
shoma115 <xiangzhendaogen8@gmail.com>
2023-12-05 11:03:53 +0900

    ルーティングの作成

--- review-app-backend/routes/api.php
+++ review-app-backend/routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LessonController;
+use App\Http\Controllers\ReviewController;
 
 /*
 |--------------------------------------------------------------------------
