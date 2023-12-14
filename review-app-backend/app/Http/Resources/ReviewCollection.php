1079ed6de93ee42a918a6b22aa97ccff8aea18df
shoma115 <xiangzhendaogen8@gmail.com>
2023-12-14 23:03:28 +0900

    レビューテーブルにユーザーの外部キーを追加

--- review-app-backend/app/Http/Resources/ReviewCollection.php
+++ review-app-backend/app/Http/Resources/ReviewCollection.php
@@ -15,7 +15,7 @@ class ReviewCollection extends ResourceCollection
     public function toArray(Request $request): array
     {
         return [
-            "reviews" => $this->collection,
+            "lesson" => $this->collection,
         ];
     }
 }
