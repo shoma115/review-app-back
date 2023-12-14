407f8da7fe79ea982e4146cc823a27b6dc87400c
shoma115 <xiangzhendaogen8@gmail.com>
2023-12-14 23:06:22 +0900

    ユーザーとレビューのリレーションを作成

--- review-app-backend/app/Models/Review.php
+++ review-app-backend/app/Models/Review.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use App\Models\Lesson;
+use App\Models\User;
 
 class Review extends Model
 {
@@ -12,5 +13,9 @@ class Review extends Model
 
     public function lessons() {
         return $this->belongsToMany(Lessn::class);
+    }
+
+    public function user() {
+        return $this->belongsTo(User::class);
     }
 }
